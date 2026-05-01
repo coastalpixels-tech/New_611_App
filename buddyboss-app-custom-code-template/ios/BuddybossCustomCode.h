de48b09c6d788c679b6a15b6b12ffed643b8ffcf
coastalpixels-tech <coastalpixels@gmail.com>
2026-04-30 20:26:41 -0400

    Delete buddyboss-app-custom-code-template directory

--- buddyboss-app-custom-code-template/ios/BuddybossCustomCode.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTRootView.h>
-
-@interface BuddybossCustomCode : NSObject
-
-+ (void)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions withBridge:(RCTBridge *)bridge;
-+ (void)rootViewVisible:(RCTRootView *)rootView;
-
-@end
